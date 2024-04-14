d7d3475851aa36b3c3300b703172010115dae2ff
alaa ben mohamed <106694845+alaabenmohamed@users.noreply.github.com>
2024-04-14 15:04:53 +0100

    feat: add `Produits` file

---
+++ src/Data.js
@@ -0,0 +1,58 @@
+import Fanta from "./produitsImage/Fanta.jpg";
+import Croissant from "./produitsImage/Croissant.jpeg";
+import Banana from "./produitsImage/Banana.jpeg";
+import gateau from "./produitsImage/gateau.jpeg";
+import Eau from "./produitsImage/Eau.jpeg";
+import Formage from "./produitsImage/Formage.jpg";
+
+const Produits = [
+  {
+    id: 1,
+    Nom: "Fanta",
+    imgUrl: Fanta,
+    prix: 10,
+    description:
+      "canette fanta pomme verte 35,5 cl ingrédients: EAU CARBONÉE, SIROP DE MAÏS À TENEUR EN FRUCTOSE ÉLEVÉ, ARÔMES NATURELS, ACIDE MALIQUE, SORBATE DE POTASSIUM ...",
+  },
+  {
+    id: 2,
+    Nom: "Croissant",
+    imgUrl: Croissant,
+    prix: 20,
+    description:
+      "Un croissant est une viennoiserie à base d'une pâte levée feuilletée spécifique, la pâte à croissant, qui comporte de la levure et une proportion importante ...",
+  },
+  {
+    id: 3,
+    Nom: "Banana",
+    imgUrl: Banana,
+    prix: 30,
+    description:
+      "La banane est le fruit ou la baie dérivant de l'inflorescence du bananier. Les bananes sont des fruits très généralement stériles issus de variétés ...",
+  },
+  {
+    id: 4,
+    Nom: "gateau",
+    imgUrl: gateau,
+    prix: 40,
+    description:
+      "Toutes les recettes de gâteaux classées par catégories : gâteaux au chocolat, charlottes, crumbles, flans, etc. des recettes faciles et délicieuses.",
+  },
+  {
+    id: 5,
+    Nom: "Eau",
+    imgUrl: Eau,
+    prix: 50,
+    description:
+      "Les eaux minérales naturelles qui contiennent du sodium (plus de 200 mg/litre) sont particulièrement recommandées aux sportifs effectuant des efforts intenses ...",
+  },
+  {
+    id: 6,
+    Nom: "Formage",
+    imgUrl: Formage,
+    prix: 60,
+    description:
+      "Les fromages au déjeuner · Qui dit matin dit fromages! , accompagné d'un café filtre et d'un morceau de pain aux noix ou ...",
+  },
+];
+export default Produits;
