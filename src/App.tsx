755416177a655be1d9fd7886f0e2faa50145cb1a
alaa ben mohamed <106694845+alaabenmohamed@users.noreply.github.com>
2024-04-14 15:08:58 +0100

    feat: add `Header` ,  `HeroPage` files , `ProviderStpre` and  `Persist` in `App` file

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import HeroPage from "./HeroPage";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <HeroPage />
+    </div>
+  );
+}
